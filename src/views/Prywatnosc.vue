cfdf32f1d7430ca3ecd49d1dbe8ad2ae7eda4ccf
Szymon Sandecki <szymonsandecki@gmail.com>
2024-07-03 21:30:14 +0200

    drugi commit

--- src/views/Prywatnosc.vue
+++ src/views/Prywatnosc.vue
@@ -11,6 +11,7 @@ export default {
       <p>Polityka prywatności określa, jak zbierane, przetwarzane i przechowywane są dane osobowe Użytkowników niezbędne do świadczenia usług drogą elektroniczną za pośrednictwem serwisu internetowego [adres strony] (dalej: Serwis).
         Serwis zbiera wyłącznie dane osobowe niezbędne do świadczenia i rozwoju usług w nim oferowanych.
         Dane osobowe zbierane za pośrednictwem Serwisu są przetwarzane zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (ogólne rozporządzenie o ochronie danych, dalej RODO) oraz ustawą o ochronie danych osobowych z dnia 10 maja 2018 r.</p>
+        <p> </p>
     </div>
 </template>
 
